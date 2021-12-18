afe77cd359cc49048afe63f7a5e4da0787bf337c
rajib4g <rajib6850@gmail.com>
2021-12-18 21:53:57 +0600

    Project 02 Done

--- project02/js/main.js
+++ project02/js/main.js
@@ -5,6 +5,30 @@ jQuery(document).ready(function() {
 
 
 
+    $('.menuIcon').click(function() {
+
+        $('.sidebarNav').toggleClass('menuActive');
+
+        if (jQuery('.sidebarNav').hasClass('menuActive')) {
+
+            jQuery('.menuActive').children('.leftMenu').children('.menuIcon').css({ 'right': '0', 'top': '0', 'left': 'inherit' })
+            jQuery('.menuActive').children('.leftMenu').children('.menuIcon').addClass('iconChange')
+
+
+        } else {
+            jQuery('.leftMenu').children('.menuIcon').css({ 'right': 'inherit', 'left': '0' })
+            jQuery('.sidebarNav').children('.leftMenu').children('.menuIcon').removeClass('iconChange')
+
+        }
+        return false;
+    });
+
+
+
+
+
+
+
 
 
 
@@ -13,14 +37,3 @@ jQuery(document).ready(function() {
 
 
 })
-
-
-
-
-
-
-
-
-
-// ami jetay click korbo oitar next() element a.active class add hoibe
-// R onno gula theke active class remove hoibe
